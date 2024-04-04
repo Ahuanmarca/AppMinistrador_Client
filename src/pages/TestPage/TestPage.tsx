36f54020df40ab29f69e449710290f984ebd64c7
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-04 23:52:29 +0200

    react-grid-layout test ok

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import GridLayout from 'react-grid-layout';
+import style from './TestPage.module.css';
 
 function TestPage() {
   const [greet, setGreet] = React.useState('');
@@ -7,7 +9,34 @@ function TestPage() {
     setGreet('hello, world');
   }, []);
 
-  return <div>{greet}</div>;
+  const layout = [
+    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+  ];
+
+  return (
+    <>
+      <h1>{greet}</h1>
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
+      >
+        <div className={style.panel} key="a">
+          a
+        </div>
+        <div className={style.panel} key="b">
+          b
+        </div>
+        <div className={style.panel} key="c">
+          c
+        </div>
+      </GridLayout>
+    </>
+  );
 }
 
 export default TestPage;
