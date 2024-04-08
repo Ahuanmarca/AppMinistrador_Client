efe343aa77cd7d2d6fe2c69315afe14a954a1904
Andrea Mir <amirolba@gmail.com>
2024-04-09 00:36:54 +0200

    Footer created

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 // import * as React from 'react';
-import LayoutPlaceholder from "../Layout/LayoutPlaceholder";
-
+// import LayoutPlaceholder from "../Layout/LayoutPlaceholder";
+import styles from './Footer.module.css'
 function Footer() {
   return (
-    <LayoutPlaceholder placeholderText={'Footer'} />
+    <div className={styles.footer}>
+      <p className={styles.brand}>AppMinistrador</p>
+      <p>Todos los derechos reservados &copy; 2024</p>
+    </div>
   );
 }
 
