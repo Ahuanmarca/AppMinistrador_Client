b45bafd0459e7e16fb657cd4949566a5b743bc8f
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-10 12:00:23 +0200

    WIP Incidences: Getting network data from DashboardContext

--- src/components/Dashboard/Incidences/Incidences.tsx
+++ src/components/Dashboard/Incidences/Incidences.tsx
@@ -1,11 +1,26 @@
-// import * as React from 'react';
+import * as React from 'react';
+import { DashboardContext } from '../../../contexts/DashboardContext';
+// import { useReactTable } from '@tanstack/react-table';
+
 import styles from './Incidences.module.css';
+import IncidencesTable from './IncidencesTable';
 
 function Incidences() {
+  // @ts-expect-error -- Property 'isLoading' does not exist on type '{}'.ts(2339)
+  const { data, error, isLoading } = React.useContext(DashboardContext);
+ 
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error</h1>;
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <p>Incidences</p>
+        <IncidencesTable incidences={data.buildingData.incidences} />
       </div>
     </div>
   );
--- src/components/Dashboard/Incidences/IncidencesTable/IncidencesTable.tsx
+++ src/components/Dashboard/Incidences/IncidencesTable/IncidencesTable.tsx
@@ -1,7 +1,32 @@
-import * as React from 'react';
+// import * as React from 'react';
 
-function IncidencesTable() {
-  return <div></div>;
+function IncidencesTable({ incidences }) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Usuario</th>
+            <th>Edificio</th>
+            <th>Estado</th>
+            <th>Descripción</th>
+          </tr>
+        </thead>
+        <tbody>
+          {incidences.map((incidence) => (
+            <tr key={incidence.id}>
+              <td>{incidence.date}</td>
+              <td>{incidence.user}</td>
+              <td>{incidence.building}</td>
+              <td>{incidence.state}</td>
+              <td>{incidence.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default IncidencesTable;
