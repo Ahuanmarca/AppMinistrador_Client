5263b5caf114e2411fbb3f6471de42c467f86ed0
Andrea Mir <amirolba@gmail.com>
2024-04-10 18:25:51 +0200

    wip

--- src/components/Appbar/Searchbar/SearchBar.tsx
+++ src/components/Appbar/Searchbar/SearchBar.tsx
@@ -2,16 +2,17 @@
 import GlassIcon from './MagnifyingGlassIconV2';
 import styles from './SearchBar.module.css';
 
-function SearchBar() {
+function SearchBar({placeholder, width, height}) {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ width: width, height: height }}>
         <div className={styles.icon}>
           <GlassIcon />
         </div>
       <input
         className={styles.search}
         type="text"
-        placeholder="Busca una finca"
+        placeholder={placeholder}
+        //"Busca una finca"
       />
     </div>
   );
--- src/components/Dashboard/Providers/Providers.tsx
+++ src/components/Dashboard/Providers/Providers.tsx
@@ -1,10 +1,19 @@
 // import * as React from 'react';
 // import DashboardPlaceholder from "../DashboardPlaceholder";
-
+import styles from './Providers.module.css'
+import SearchBar from '../../Appbar/Searchbar/SearchBar';
 function Providers() {
   return (
     // <DashboardPlaceholder placeholderText={"Proveedores"} />
-  <></>);
+  <div className={styles.container}>
+    <div className={styles.titleWrapper}>
+      <h1 className={styles.title}>Proveedores</h1>
+    </div>
+    <div className={styles.searchWrapper}>
+      <h2 className={styles.searchTitle}>Nombre</h2>
+      <SearchBar width={'100%'} height={'34px'} placeholder={'Ej: Érika López'}></SearchBar>
+    </div>
+  </div>);
 }
 
 export default Providers;
