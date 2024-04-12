43bebbd955e92bcfc43722caef76c180d993ce74
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-12 15:27:56 +0200

    Add loader spinner to incidences

--- src/components/Dashboard/Incidences/Incidences.tsx
+++ src/components/Dashboard/Incidences/Incidences.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { DashboardContext } from '../../../contexts/DashboardContext'
-import{ Incidence } from '../../../types/Incidence';
+import { DashboardContext } from '../../../contexts/DashboardContext';
+import { Incidence } from '../../../types/Incidence';
 import styles from './Incidences.module.css';
 import { formatDateTime } from './helpers';
+import { Circles } from 'react-loader-spinner';
 
 import IncidencesTable from './IncidencesTable';
 
@@ -11,24 +12,37 @@ function Incidences() {
   const { data, error, isLoading } = React.useContext(DashboardContext);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <Circles color="var(--brand-color-main)" height={250} width={100} />;
+      </div>
+    ) 
   }
 
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
-  const incidences: Array<Incidence> = data.buildingData.incidences.map((incidence) => {
-    return {
-      id: incidence.id,
-      title: incidence.title,
-      description: incidence.description,
-      date: formatDateTime(incidence.date, incidence.time),
-      category: incidence.category,
-      user: incidence.users.people.forename,
-      status: incidence.status,
-    };
-  });
+  const incidences: Array<Incidence> = data.buildingData.incidences.map(
+    (incidence) => {
+      return {
+        id: incidence.id,
+        title: incidence.title,
+        description: incidence.description,
+        date: formatDateTime(incidence.date, incidence.time),
+        category: incidence.category,
+        user: incidence.users.people.forename,
+        status: incidence.status,
+      };
+    }
+  );
 
   console.debug(incidences[0]);
 
