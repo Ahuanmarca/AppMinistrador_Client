2d00c73904e5114ed11adb3c8d4ff9b7760163b2
Andrea Mir <amirolba@gmail.com>
2024-04-13 20:41:07 +0200

    Contabilidad page with both components and Resize Bar included

--- src/pages/ContabilidadPage/ContabilidadPage.tsx
+++ src/pages/ContabilidadPage/ContabilidadPage.tsx
@@ -1,8 +1,38 @@
 // import * as React from 'react';
-import PagePlaceholder from "../PagePlaceholder";
+// import PagePlaceholder from "../PagePlaceholder";
+import ResizeHandle from '../../components/Dashboard/ResizeHandle';
+import styles from './ContabilidadPage.module.css';
+import MonthFees from "../../components/Dashboard/MonthFees";
+import CashflowHistogram from '../../components/Dashboard/CashflowHistogram';
+import { PanelGroup, Panel } from 'react-resizable-panels';
 
 function ContabilidadPage() {
-  return <PagePlaceholder placeholderText="Contabilidad" />
+  return(
+    <div className={styles.container}>
+      <h1 className={styles.title}>Contabilidad</h1>
+    <PanelGroup direction={'horizontal'}>
+      {/* <div className={styles.container}> */}
+        <Panel defaultSize={50} minSize={30}>
+          {/* <div className={styles.pieContainer}> */}
+            <MonthFees></MonthFees>
+          {/* </div> */}
+        </Panel>
+        <ResizeHandle className={styles.handle}></ResizeHandle>
+        <Panel defaultSize={50} minSize={40}>
+          {/* <div className={styles.diagramContainer}> */}
+            <CashflowHistogram></CashflowHistogram>
+          {/* </div> */}
+        </Panel>
+      {/* </div> */}
+    </PanelGroup>
+    <div className={styles.data}></div>
+    {/* <PanelGroup direction={'horizontal'}>
+      
+    </PanelGroup> */}
+    </div>
+  ) 
+
+  
 }
 
 export default ContabilidadPage;
