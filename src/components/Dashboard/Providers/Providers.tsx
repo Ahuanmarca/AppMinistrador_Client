79daea0410535f03e2c41b4b12b3c4336e258a6e
Andrea Mir <amirolba@gmail.com>
2024-04-13 09:09:15 +0200

    Fixing % of container and CSS of SearchBar

--- src/components/Dashboard/Providers/Providers.tsx
+++ src/components/Dashboard/Providers/Providers.tsx
@@ -1,7 +1,8 @@
 // import * as React from 'react';
 // import DashboardPlaceholder from "../DashboardPlaceholder";
 import styles from './Providers.module.css'
-import SearchBar from '../../Appbar/Searchbar/SearchBar';
+import SearchBarProviders from './SearchBarProviders';
+// import SearchBar from '../../Appbar/Searchbar/SearchBar';
 function Providers() {
   return (
     // <DashboardPlaceholder placeholderText={"Proveedores"} />
@@ -11,7 +12,7 @@ function Providers() {
     </div>
     <div className={styles.searchWrapper}>
       <h2 className={styles.searchTitle}>Nombre</h2>
-      <SearchBar width={'100%'} height={'34px'} placeholder={'Ej: Érika López'}></SearchBar>
+      { <SearchBarProviders width={'90%'} height={'34px'} placeholder={'Ej: Electricista Lumix'}></SearchBarProviders>}
     </div>
   </div>);
 }
