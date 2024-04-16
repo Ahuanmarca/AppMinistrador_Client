5dec6dc2f682508f42886333796203c05d17d82a
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-16 14:46:14 +0200

    Report Incidence Form Modal - No request yet

--- src/components/ReportIncidence/ReportIncidence.tsx
+++ src/components/ReportIncidence/ReportIncidence.tsx
@@ -33,68 +33,76 @@ function ReportIncidence() {
   }
 
   return (
-    <div className={styles.wrapper}>
-      <Form.Root className={styles.FormRoot}>
-        <Form.Field className={styles.FormField} name="title">
-          <Form.Label>Título</Form.Label>
-          <Form.Message match="valueMissing">
-            Por favor introduzca un título
-          </Form.Message>
-          <Form.Control
-            className={styles.TextInput}
-            asChild
-            value={formValues.title}
-            onChange={handleChange}
-          >
-            <input type="text" required />
-          </Form.Control>
-        </Form.Field>
+    <Form.Root className={styles.FormRoot}>
 
-        <Form.Field className={styles.FormField} name="description">
-          <Form.Label>Descripción</Form.Label>
-          <Form.Message match="valueMissing">
-            Por favor introduzca una descripción
-          </Form.Message>
-          <Form.Control
-            className={styles.TextInput}
-            asChild
-            value={formValues.description}
-            onChange={handleChange}
-          >
-            <textarea rows={4} required />
-          </Form.Control>
-        </Form.Field>
+      <Form.Field
+        className={`${styles.FormField} ${styles.Title}`}
+        name="title"
+      >
+        <Form.Label>Título</Form.Label>
+        <Form.Control
+          className={styles.TextInput}
+          asChild
+          value={formValues.title}
+          onChange={handleChange}
+        >
+          <input type="text" required />
+        </Form.Control>
+        <Form.Message className={styles.ErrorMessage} match="valueMissing">
+          Por favor introduzca un título
+        </Form.Message>
+      </Form.Field>
 
-        <Form.Field className={styles.FormField} name="category">
-          <Form.Label>Categoría</Form.Label>
-          <Form.Message match="valueMissing">
-            Por favor seleccione una categoría
-          </Form.Message>
-          <Form.Control
-            className={styles.TextInput}
-            required
-            asChild
-            onChange={handleChange}
-            // value={formValues.category}
-          >
-            <select>
-              <option value="">- Selecciona una categoría -</option>
-              <optgroup label="Categorías">
-                {CATEGORIES.map((category, index) => (
-                  <option key={index} value={category}>
-                    {category}
-                  </option>
-                ))}
-              </optgroup>
-            </select>
-          </Form.Control>
-        </Form.Field>
+      <Form.Field
+        className={`${styles.FormField} ${styles.Description}`}
+        name="description"
+      >
+        <Form.Label>Descripción</Form.Label>
+        <Form.Control
+          className={styles.TextInput}
+          asChild
+          value={formValues.description}
+          onChange={handleChange}
+        >
+          <textarea rows={4} required />
+        </Form.Control>
+        <Form.Message className={styles.ErrorMessage} match="valueMissing">
+          Por favor introduzca una descripción
+        </Form.Message>
+      </Form.Field>
 
-        <Form.Submit className={styles.Submit} asChild>
-          <button>Reportar</button>
-        </Form.Submit>
-      </Form.Root>
-    </div>
+      <Form.Field
+        className={`${styles.FormField} ${styles.Category}`}
+        name="category"
+      >
+        <Form.Label>Categoría</Form.Label>
+        <Form.Control
+          className={styles.Dropdown}
+          required
+          asChild
+          onChange={handleChange}
+          // value={formValues.category}
+        >
+          <select>
+            <option value="">- Selecciona una categoría -</option>
+            <optgroup label="Categorías">
+              {CATEGORIES.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </optgroup>
+          </select>
+        </Form.Control>
+        <Form.Message className={styles.ErrorMessage} match="valueMissing">
+          Por favor seleccione una categoría
+        </Form.Message>
+      </Form.Field>
+
+      <Form.Submit className={styles.Submit} asChild>
+        <button>Reportar</button>
+      </Form.Submit>
+    </Form.Root>
   );
 }
 
