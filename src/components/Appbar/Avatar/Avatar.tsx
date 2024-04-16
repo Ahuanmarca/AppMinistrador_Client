56e2423076c15529dcdb5383a421326b37364b59
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-16 22:30:33 +0200

    User Avatar shows logged in user picture

--- src/components/Appbar/Avatar/Avatar.tsx
+++ src/components/Appbar/Avatar/Avatar.tsx
@@ -3,10 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import * as AvatarRadix from '@radix-ui/react-avatar';
 import styles from './Avatar.module.css';
 
-const imageURL = 'https://images.pexels.com/photos/11531404/pexels-photo-11531404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+const fallbackImageUrl = 'https://images.pexels.com/photos/11531404/pexels-photo-11531404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+const UNSPLASH_BASE_URL = 'https://images.unsplash.com/photo-';
+const UNSPLASH_QUERY_PARAM = '?q=80&w=800';
 
 function Avatar() {
   const navigate = useNavigate(); // Obtiene la función navigate
+  const user = JSON.parse(localStorage.getItem('user') || '{}' );
+  const imageURL = user
+    ? `${UNSPLASH_BASE_URL}${user.portrait_url}${UNSPLASH_QUERY_PARAM}`
+    : fallbackImageUrl;
 
   const handleClick = () => {
     navigate('/perfil'); 
