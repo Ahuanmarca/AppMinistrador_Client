e4f47f6ad1556438eff8d48e4f53ac5e4f6f1c5b
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-18 19:52:14 +0200

    Adjust debug incidence form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -17,7 +17,23 @@ function App() {
       <DashboardProvider>
         <Routes>
           <Route path="/login" element={<AuthForm />} />
-          <Route path="/mobile" element={<ReportIncidence handleDismiss={() => console.log("Reported")}/>} />
+          <Route path="/mobile" element={
+          
+          <div
+            style={{
+              height: "100svh",
+              width: "100%",
+              padding: "2rem 1rem",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <ReportIncidence handleDismiss={() => console.log("Reported")}/>
+          </div>
+        
+        } />
           <Route path="/tester">
             <Route path="/tester/basic" element={<Basic />} />
             <Route path="/tester/incidences" element={<Incidences />} />
